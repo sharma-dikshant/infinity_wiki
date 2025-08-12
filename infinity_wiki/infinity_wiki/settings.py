48870c93fbf35a6b9cdcc5ab49736be21a6f688b
sharma-dikshant <dikshantsharma2005@gmail.com>
2025-08-12 09:22:40 +0530

    implemented genai

--- infinity_wiki/infinity_wiki/settings.py
+++ infinity_wiki/infinity_wiki/settings.py
@@ -9,8 +9,13 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+from decouple import config
+
+
+
+GEMINI_API_KEY = config('GEMINI_API_KEY')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -55,7 +60,7 @@ ROOT_URLCONF = 'infinity_wiki.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS=[os.path.join(BASE_DIR, 'static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
--- infinity_wiki/wiki/views.py
+++ infinity_wiki/wiki/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.conf import settings
+
+import google.generativeai as genai
+
+
+genai.configure(api_key=settings.GEMINI_API_KEY)
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse("hello infinity wiki")
+    result = None
+    if request.method == 'POST':
+        query = request.POST.get('query')
+        if query:
+            model = genai.GenerativeModel("gemini-1.5-flash-002")
+            prompt = (
+                "You are like a Wikipedia, so whatever the user query is, "
+                "explain it thoroughly in an easy-to-understand way. "
+                "Give context, and if examples are possible, also provide examples. "
+                "Additionally, give related information for better understanding.\n\n"
+                f"User query: {query}"
+            )
+            response = model.generate_content(prompt)
+            result = response.text
+            
+    return render(request, 'wiki/home.html', {'result': result})
